1a527e72d288f6a1fe09a584487d92c369ac2313
NikitinAA <nik.ant38@mail.ru>
2020-06-01 15:29:28 +0300

    first commit

--- test.py
+++ test.py
@@ -1,23 +1,19 @@
-import time
 import sys
 import subprocess
 
-#тест1
+#тест1 аргумент 50
 code = subprocess.run('python project.py 50', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL);
 if(code.returncode == 0):
-    print('Тест 1 прошел успешно')
+    print('Test 1 proshel uspeshno')
 else:
-    print('Тест 1 ошибка')
+    print('Test 1 error')
     sys.exit(1)
 
-#тест2
-code = subprocess.run('python project.py Карусель', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL);
+#тест2 аргумент 5
+code = subprocess.run('python project.py 5', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL);
 if(code.returncode == 0):
-    print('Тест 2 прошел успешно')
+    print('Test 2 proshel uspeshno')
 else:
-    print('Тест 2 ошибка')
+    print('Test 2 error')
     sys.exit(1)
-
-
-# code = subprocess.run(['C:\python\python.exe', 'sub.py', '50'], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
